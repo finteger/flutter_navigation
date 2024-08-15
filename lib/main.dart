8392df14d51b433ca4b6ad13d979b4b06a43b1c0
finteger <nashassociatesinc@gmail.com>
2024-08-15 13:54:34 -0600

    Update

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Named Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/secondpage': (context) => SecondPage(),
+        '/thirdpage': (context) => ThirdPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: Text('Home Page'),
+    ));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: Text('Second Page'),
+    ));
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: Text('Third Page'),
+    ));
+  }
+}
